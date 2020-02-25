c8fd982e9ccd749f61483454c636b2e2359ebf6d
Turchynyk Oleksii <mishkalesha1994@gmail.com>
2020-02-25 10:26:35 +0200

    my first react.js app

---
+++ precache-manifest.77c3793083b79ac380e5d4915e176f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d76f363bb2a2037b8fbb318b1f0f326",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0468df4e726a88e4a53f",
+    "url": "./static/css/main.6aef40d8.chunk.css"
+  },
+  {
+    "revision": "21855759815f5c3c3314",
+    "url": "./static/js/2.66484930.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.66484930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0468df4e726a88e4a53f",
+    "url": "./static/js/main.4bc202d5.chunk.js"
+  },
+  {
+    "revision": "15e26150d4fcf60035a0",
+    "url": "./static/js/runtime-main.bd2e8670.js"
+  }
+]);
